b71dd9226a99309109215428dfc70ab3ab1aa0bc
razimograbi <razi@mograbi.net>
2024-03-02 14:57:20 +0200

    added correct cors

--- backend/server.js
+++ backend/server.js
@@ -1,52 +1,57 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cookieParser = require('cookie-parser');
-const dotenv = require('dotenv').config(); // to make server.js have access to .env file .
-const connectDB = require('./config/connectDB');
+const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv").config(); // to make server.js have access to .env file .
+const connectDB = require("./config/connectDB");
 const PORT = process.env.PORT || 2501;
-const cors = require('cors');
+const cors = require("cors");
 
 // middleware functions
-const cookieJwtAuth = require('./middleware/cookieJwtAuth');
-const fetchUserData = require('./middleware/fetchUserData');
+const cookieJwtAuth = require("./middleware/cookieJwtAuth");
+const fetchUserData = require("./middleware/fetchUserData");
 
 // routes import
-const loginRoute = require('./routes/login');
-const registerRoute = require('./routes/register');
-const userHomeRouter = require('./routes/userHome');
-const expensesRouter = require('./routes/expenses');
-const budgetRouter = require('./routes/budget');
-const goalsRouter = require('./routes/goals');
+const loginRoute = require("./routes/login");
+const registerRoute = require("./routes/register");
+const userHomeRouter = require("./routes/userHome");
+const expensesRouter = require("./routes/expenses");
+const budgetRouter = require("./routes/budget");
+const goalsRouter = require("./routes/goals");
 
 // middleWare
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors()); // allow any one to send you http requests
+app.use(
+  cors({
+    origin: [
+      "http://localhost:2501",
+      "https://PartialBackendForWeb.onrender.com",
+    ],
+  })
+); // allow any one to send you http requests
 
 //routing
-app.use('/login', loginRoute);
-app.use('/register', registerRoute);
-app.use('/pages/userHome', [cookieJwtAuth, fetchUserData] , userHomeRouter);
-app.use('/pages/api/expenses', [cookieJwtAuth, fetchUserData] , expensesRouter);
-app.use('/pages/api/budget', [cookieJwtAuth, fetchUserData], budgetRouter);
-app.use('/pages/api/goals' ,cookieJwtAuth ,goalsRouter);
+app.use("/login", loginRoute);
+app.use("/register", registerRoute);
+app.use("/pages/userHome", [cookieJwtAuth, fetchUserData], userHomeRouter);
+app.use("/pages/api/expenses", [cookieJwtAuth, fetchUserData], expensesRouter);
+app.use("/pages/api/budget", [cookieJwtAuth, fetchUserData], budgetRouter);
+app.use("/pages/api/goals", cookieJwtAuth, goalsRouter);
 
-
-app.get('/', (req, res) => {
-    res.status(200).json({"success" : true});
+app.get("/", (req, res) => {
+  res.status(200).json({ success: true });
 });
 
-
 const startServer = async () => {
-    try {
-        await connectDB();
-        app.listen(PORT, ()=>{
-            console.log(`server is running on port ${PORT}`);
-        });
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 startServer();
