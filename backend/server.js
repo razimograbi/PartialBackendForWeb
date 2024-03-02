b23c6e650fae369dccc15464954a800ae46efdf2
razimograbi <razi@mograbi.net>
2024-03-02 17:30:56 +0200

    fit

--- backend/server.js
+++ backend/server.js
@@ -22,11 +22,7 @@ const goalsRouter = require("./routes/goals");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(
-  cors({
-    credentials: true,
-  })
-); // allow any one to send you http requests
+app.use(cors()); // allow any one to send you http requests
 
 //routing
 app.use("/login", loginRoute);
